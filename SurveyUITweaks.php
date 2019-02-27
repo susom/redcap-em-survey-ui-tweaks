7374c4102040ed0c162a6ad6414d6c61e368a3a8
Andrew Martin <andy123@stanford.edu>
2019-02-27 15:55:39 -0800

    updated logging and made tweaks survey-specific

--- SurveyUITweaks.php
+++ SurveyUITweaks.php
@@ -1,94 +1,130 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jael
- * Date: 10/2/17
- * Time: 2:47 PM
- */
-
 namespace Stanford\SurveyUITweaks;
+
+include_once ("emLoggerTrait.php");
 
 class SurveyUITweaks extends \ExternalModules\AbstractExternalModule
 {
+    use emLoggerTrait;
 
-    function hook_survey_complete() {
-        $hide_complete = $this->getProjectSetting('hide_queue_end');
+    public $settings;
 
-        if ($hide_complete) {
+    function __construct()
+    {
+        parent::__construct();
 
-            ?>
-            <style>
-                #survey_queue {
-                    display: none !important;
-                }
-            </style>
-            <?php
+        if ($this->getProjectId()) {
 
+            // In project context
+            $this->settings = $this->getSubSettings('survey_tweaks');
         }
     }
 
-    function hook_survey_page_top()
+    function redcap_survey_page_top($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance)
     {
-        $hide_corner = $this->getProjectSetting('hide_queue_corner');
-        $remove_excess_td = $this->getProjectSetting('remove_excess_td');
-        $hide_submit_button = $this->getProjectSetting('hide_submit_button');
-        $rename_submit_button = $this->getProjectSetting('rename_submit_button');
-
-        //hide the survey_queue button on upper right corner
-        if ($hide_corner) {
-            ?>
-            <style>
-                #return_corner, #survey_queue_corner {
-                    display: none !important;
-                }
-            </style>
-
-            <?php
+        $this->emDebug("SURVEY PAGE TOP - $instrument");
+        foreach ($this->settings as $settings) {
+            if ($settings['survey_name'] == $instrument) {
+                $this->emDebug("Running tweaks on $instrument for " . __FUNCTION__);
+                $this->runTweaks(__FUNCTION__, $settings);
+            }
         }
 
-        //remove the excess TD on left if $question_auto_numbering on
-        if ($remove_excess_td) {
+    }
 
-            global $question_auto_numbering;
-            if ($question_auto_numbering == 0) {
+
+    function redcap_survey_complete($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance)
+    {
+        foreach ($this->settings as $settings) {
+            if ($settings['survey_name'] == $instrument) {
+                $this->emDebug("Running tweaks on $instrument for " . __FUNCTION__);
+                $this->runTweaks(__FUNCTION__, $settings);
+            }
+        }
+    }
+
+    function runTweaks($function, $settings) {
+
+        // Convert the keys of settings into variables
+        $hide_queue_corner    = @$settings['hide_queue_corner'];
+        $remove_excess_td     = @$settings['remove_excess_td'];
+        $hide_submit_button   = @$settings['hide_submit_button'];
+        $rename_submit_button = @$settings['rename_submit_button'];
+        $hide_end_queue       = @$settings['hide_end_queue'];
+
+        if ($function == 'redcap_survey_page_top')
+        {
+            //hide the survey_queue button on upper right corner
+            if ($hide_queue_corner) {
                 ?>
                 <style>
-                    td.questionnum, td.questionnummatrix {
+                    #return_corner, #survey_queue_corner {
+                        display: none !important;
+                    }
+                </style>
+                <?php
+            }
+
+            //remove the excess TD on left if $question_auto_numbering on
+            if ($remove_excess_td) {
+                global $question_auto_numbering;
+                if ($question_auto_numbering == 0) {
+                    ?>
+                    <style>
+                        td.questionnum, td.questionnummatrix {
+                            display: none !important;
+                        }
+                    </style>
+                    <?php
+                }
+            }
+
+            if ($hide_submit_button) {
+                ?>
+                <script type="text/javascript">
+                    $(document).ready(function () {
+                        $("[name=submit-btn-saverecord]").hide();
+                        // //change all the submit buttons to next page buttons
+                        // $("button:contains('Submit')").text("Next Page >>");
+                    });
+                </script>
+                <?php
+            }
+
+            if ($rename_submit_button) {
+                // Hide the TR so you don't see the text get swapped out
+                ?>
+                <style>
+                    tr.surveysubmit {
+                        opacity: 0;
+                    }
+                </style>
+                <script type="text/javascript">
+                    $(document).ready(function () {
+                        var newval = "<?php echo $rename_submit_button ?>";
+                        //change all the submit buttons to next page buttons
+                        $("button:contains('Submit')").text(newval);
+                        $("tr.surveysubmit").css({"opacity":1});
+                    });
+                </script>
+                <?php
+
+            }
+
+        }
+
+        if ($function == 'redcap_survey_complete')
+        {
+            if ($hide_end_queue) {
+                ?>
+                <style>
+                    #survey_queue {
                         display: none !important;
                     }
                 </style>
                 <?php
             }
         }
-
-        if ($hide_submit_button) {
-            ?>
-            <script type="text/javascript">
-                $(document).ready(function() {
-
-                    $("[name=submit-btn-saverecord]").hide();
-
-                    //change all the submit buttons to next page buttons
-                    $("button:contains('Submit')").text("Next Page >>");
-                });
-            </script>
-            <?php
-
-        }
-
-        if ($rename_submit_button) {
-            ?>
-            <script type="text/javascript">
-                $(document).ready(function() {
-                    var newval = "<?php echo $rename_submit_button ?>";
-
-
-                    //change all the submit buttons to next page buttons
-                    $("button:contains('Submit')").text(newval);
-                });
-            </script>
-            <?php
-
-        }
     }
+
 }
